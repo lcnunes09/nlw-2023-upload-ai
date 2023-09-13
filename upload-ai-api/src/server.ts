b7b3bc3c25e3082027e0d8709a70f8b3c0c07ed5
Luciana Nunes <lcnunes09@gmail.com>
2023-09-13 00:22:07 -0500

    chore: add fastify cors

--- upload-ai-api/src/server.ts
+++ upload-ai-api/src/server.ts
@@ -1,10 +1,15 @@
 import { fastify } from "fastify"
+import { fastifyCors } from "@fastify/cors"
 import { getAllPromptsRoutes } from "./routes/get-all-prompts"
 import { uploadVideoRoutes } from "./routes/upload-video"
 import { createTranscriptionsRoute } from "./routes/create-transcriptions"
 import { generateAICompletionRoute } from "./routes/generate-ai-completion"
 
 export const app = fastify()
+
+app.register(fastifyCors, {
+    origin: '*',
+})
 
 app.register(getAllPromptsRoutes)
 app.register(uploadVideoRoutes)
